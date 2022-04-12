5b219bad6c99b5ebd256143f99fb513099f20ea0
David Albert <d.albert015@yahoo.com>
2022-04-12 14:27:30 +0300

    social login

--- app/src/main/java/com/example/android_project/MainActivity.java
+++ app/src/main/java/com/example/android_project/MainActivity.java
@@ -1,14 +1,63 @@
 package com.example.android_project;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView info;
+    private ImageView profile;
+    private LoginButton login;
+    CallbackManager callbackManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        info = findViewById(R.id.info);
+        profile = findViewById(R.id.profile);
+        login = findViewById(R.id.login);
+
+        callbackManager = CallbackManager.Factory.create();
+
+        login.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                info.setText("User Id: " + loginResult.getAccessToken().getUserId());
+                String imageUrl = "https://graph.facebook.com/" +
+                        loginResult.getAccessToken().getUserId() +
+                        "/picture?return_ssl_resources=1";
+                Picasso.get().load(imageUrl).into(profile);
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(@NonNull FacebookException e) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
